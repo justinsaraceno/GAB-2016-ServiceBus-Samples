7cb92a8e5604753a5d3ef6f764b9599e5ae365ed
Justin Saraceno <justin.saraceno@gmail.com>
2016-04-14 13:01:17 -0400

    adding notice queue sample

---
+++ QueueReceiver/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueReceiver
+{
+    using Microsoft.ServiceBus;
+    using Microsoft.ServiceBus.Messaging;
+    using Microsoft.WindowsAzure;
+
+    class Program
+    {
+        private static string busConnectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+        private static NamespaceManager namespaceManager = NamespaceManager.CreateFromConnectionString(busConnectionString);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Service Bus Receiver Ready! (PRETEND THIS SENDS NOTIFICATIONS)");
+            Console.WriteLine("Listening for incoming messages...");
+            ConsumeIncomingMessages();
+        }
+
+        static void ConsumeIncomingMessages()
+        {
+            QueueClient client = QueueClient.CreateFromConnectionString(busConnectionString, "NoticeQueue");
+
+            // Configure the callback options.
+            OnMessageOptions options = new OnMessageOptions();
+            options.AutoComplete = false;
+            options.AutoRenewTimeout = TimeSpan.FromMinutes(1);
+
+            // Callback to handle received messages.
+            client.OnMessage((message) =>
+            {
+                try
+                {
+                    // Process message from queue.
+                    Console.WriteLine("Message Body: " + message.GetBody<string>());
+                    Console.WriteLine("Message Id: " + message.MessageId);
+                    Console.WriteLine("[Property] To: " + message.Properties["To"]);
+                    Console.WriteLine("[Property] Subject: " + message.Properties["Subject"]);
+                    Console.WriteLine("[Property] Body: " + message.Properties["Body"]);
+
+                    //throw new Exception("some exception to keep things in the queue");
+
+                    // Remove message from queue.
+                    Console.WriteLine("   +++Removing message from queue+++");
+                    message.Complete();
+                    Console.WriteLine(" ");
+                }
+                catch (Exception)
+                {
+                    // Indicates a problem, unlock message in queue.
+                    message.Abandon();
+                    Console.WriteLine("failed!");
+                }
+            }, options);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ QueueSender/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace QueueSender
+{
+    using Microsoft.ServiceBus;
+    using Microsoft.ServiceBus.Messaging;
+    using Microsoft.WindowsAzure;
+
+    class Program
+    {
+        private static string busConnectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+        private static NamespaceManager namespaceManager = NamespaceManager.CreateFromConnectionString(busConnectionString);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Service Bus Queue Sender Ready! (PRETEND THIS HANDLES QUEUING EMAILS)");
+            
+            while (true)
+            {
+                Console.WriteLine("Press any key to send a Notification message to the queue... ");
+                var input = Console.ReadKey();
+                SendMessageToQueue();
+                Console.WriteLine($"\n ++Message Sent at: {DateTime.Now}++");
+                Console.WriteLine(" ");
+            }
+        }
+
+        static void SendMessageToQueue()
+        {
+            // set-up queue
+            if (!namespaceManager.QueueExists("NoticeQueue"))
+            {
+                namespaceManager.CreateQueue("NoticeQueue");
+            }
+
+            // send message to queue
+            QueueClient client = QueueClient.CreateFromConnectionString(busConnectionString, "NoticeQueue");
+            DateTime messageDateTime = DateTime.Now;
+            BrokeredMessage message = new BrokeredMessage("Notification Message " + messageDateTime);
+            message.Properties["To"] = "justin.saraceno@gmail.com";
+            message.Properties["Subject"] = "This is message " + messageDateTime;
+            message.Properties["Body"] = "Hello world, this is a test message.";
+            client.Send(message);
+        }
+    }
+}
